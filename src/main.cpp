fba05675d69e10f2858c4f41ac040581cdc1242d
Jonah Morgan <jonah.allen.morgan@gmail.com>
2024-09-06 18:00:26 -0500

    added error checking

--- src/main.cpp
+++ src/main.cpp
@@ -12,42 +12,66 @@
 #include "shader.h"
 
 #define SHOW_CWD false
+#define GLCall(x)   \
+    GLClearError(); \
+    x;              \
+    GLLogCall(#x, __FILE__, __LINE__);
 
+static void GLClearError()
+{
+    while (glGetError() != GL_NO_ERROR)
+        ;
+}
 
-static void error_callback(int error, const char* description)
+static bool GLLogCall(const char *function, const char *file, int line)
+{
+    while (GLenum error = glGetError())
+    {
+        std::cout << "[OpenGL Error] (" << error << ")" << std::endl
+                  << "Function: " << function << std::endl
+                  << "File: " << file << std::endl
+                  << "Line: " << line
+                  << std::endl;
+        return false;
+    }
+    return true;
+}
+
+static void error_callback(int error, const char *description)
 {
     fprintf(stderr, "Error: %s\n", description);
 }
- 
 
-static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+static void key_callback(GLFWwindow *window, int key, int scancode, int action, int mods)
 {
     if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
         glfwSetWindowShouldClose(window, GLFW_TRUE);
 }
- 
 
 int main(void)
 {
 #if SHOW_CWD
     char cwd[PATH_MAX];
-    if (getcwd(cwd, sizeof(cwd)) != nullptr) {
+    if (getcwd(cwd, sizeof(cwd)) != nullptr)
+    {
         std::cout << "Current working directory: " << cwd << std::endl;
-    } else {
+    }
+    else
+    {
         perror("getcwd() error");
     }
 #endif
 
     glfwSetErrorCallback(error_callback);
- 
+
     if (!glfwInit())
         exit(EXIT_FAILURE);
- 
+
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
- 
-    GLFWwindow* window = glfwCreateWindow(1280, 720, "OpenGL Triangle", NULL, NULL);
+
+    GLFWwindow *window = glfwCreateWindow(1280, 720, "OpenGL Triangle", NULL, NULL);
     if (!window)
     {
         fprintf(stderr, "error while initializing window\n");
@@ -58,11 +82,12 @@ int main(void)
     int width, height;
     glfwGetFramebufferSize(window, &width, &height);
     glViewport(0, 0, width, height);
- 
+
     glfwSetKeyCallback(window, key_callback);
     glfwMakeContextCurrent(window);
-    
-    if(glewInit() != GLEW_OK) {
+
+    if (glewInit() != GLEW_OK)
+    {
         fprintf(stderr, "error while initializing glew\n");
         glfwTerminate();
         exit(EXIT_FAILURE);
@@ -74,15 +99,14 @@ int main(void)
 
     float positions[8] = {
         -0.5f, -0.5f, // 0
-         0.5f, -0.5f, // 1
-         0.5f,  0.5f, // 2
-        -0.5f,  0.5f, // 3
+        0.5f, -0.5f,  // 1
+        0.5f, 0.5f,   // 2
+        -0.5f, 0.5f,  // 3
     };
 
     unsigned int indices[] = {
         0, 1, 2,
-        2, 3, 0
-    };
+        2, 3, 0};
 
     unsigned int vao;
     glGenVertexArrays(1, &vao);
@@ -100,29 +124,26 @@ int main(void)
     glGenBuffers(1, &ibo);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
- 
+
     ShaderProgramSource source = ParseShader("../res/shaders/Basic.shader");
     unsigned int shader = CreateShader(source.vertexShader, source.fragmentShader);
 
-    std::cout << source.vertexShader << std::endl;
-    std::cout << source.fragmentShader << std::endl;
-
     glUseProgram(shader);
-    
+
     while (!glfwWindowShouldClose(window))
     {
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
- 
+        GLCall(glDrawElements(GL_TRIANGLES, 6, GL_INT, nullptr));
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
     glDeleteProgram(shader);
- 
+
     glfwDestroyWindow(window);
- 
+
     glfwTerminate();
     exit(EXIT_SUCCESS);
 }
